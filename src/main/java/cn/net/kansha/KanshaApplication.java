df9ec262dbbbd9fd0cc18e6701adff2af950fddc
Pzy <1195999342@qq.com>
2019-06-03 22:12:14 +0800

    新项目提交

---
+++ src/main/java/cn/net/kansha/KanshaApplication.java
@@ -0,0 +1,13 @@
+package cn.net.kansha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanshaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanshaApplication.class, args);
+    }
+
+}
